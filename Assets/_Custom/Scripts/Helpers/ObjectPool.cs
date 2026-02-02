417a9ba4796ea396074810d10c71b4126a3c839f
Kelvin Wijkniet <kelvinwijkniet.kw@gmail.com>
2026-02-02 20:49:07 +0100

    Cleanup

--- Assets/_Custom/Scripts/Helpers/ObjectPool.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-
-public class ObjectPool<T> where T : class, new()
-{
-    private readonly Stack<T> pool = new Stack<T>();
-
-    public T Get() => pool.Count > 0 ? pool.Pop() : new T();
-    public void Return(T obj) => pool.Push(obj);
-}
